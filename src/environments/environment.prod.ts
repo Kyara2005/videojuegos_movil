bbb7361bc63249fe7b84e2574d0e2cff591fee32
Kyara <kyara.altamirano@epn.edu.ec>
2026-05-15 16:51:24 -0500

    estilos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  supabaseUrl: 'https://haubjiclkskymrpuuqph.supabase.co',
+  supabaseKey: 'sb_publishable_8jRvVFlijFQk6ZFmG8_TNQ_NcrOPnDE',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://haubjiclkskymrpuuqph.supabase.co',
+  supabaseKey: 'sb_publishable_8jRvVFlijFQk6ZFmG8_TNQ_NcrOPnDE',
 };
 
 /*
